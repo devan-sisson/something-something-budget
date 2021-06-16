9004c720394c1531ec08c4c4a98d4ae48ba97d46
Devan Sisson <devanslink@gmail.com>
2021-06-16 12:51:23 -0600

    add api file; add budget file with definition of basic functions

---
+++ src/api/budget.js
@@ -0,0 +1,15 @@
+/**
+ * What are the basic attributes of a budget?
+ * 
+ *  - budget items
+ *  - 
+ * 
+ * 
+ * better yet: 
+ * as a user I can:
+ *  - build a list of monthly budget items
+ *  - define how many paychecks I have to work with
+ *  - determine which budget item is paid by which paycheck
+ *  - "check off" if a budget Item has been funded
+ *  - "check off" if a budget Item has been paid
+ */
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+const api = (domain) => {
+
+};
+
+export default api();
